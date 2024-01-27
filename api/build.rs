b7e63485d0113a38ca0e145b6fdc74142a8af6a5
Pandicon <70060103+Pandicon@users.noreply.github.com>
2024-01-27 13:38:48 +0100

    [API] Apply Cargo clippy suggestions

--- api/build.rs
+++ api/build.rs
@@ -17,39 +17,37 @@ fn main() {
 	fn read_folder(path: &std::path::Path, routes: &mut Vec<String>, output: &mut String, permissions_flags: &std::collections::HashMap<String, u32>) {
 		if path.is_dir() {
 			if let Ok(entries) = std::fs::read_dir(path) {
-				for entry in entries {
-					if let Ok(entry) = entry {
-						let path = entry.path();
-						read_folder(&path, routes, output, permissions_flags);
-					}
+				for entry in entries.flatten() {
+					let path = entry.path();
+					read_folder(&path, routes, output, permissions_flags);
 				}
 			}
 		} else if path.is_file() && path.extension().unwrap_or_default() == "rs" && path.file_stem().unwrap_or_default() != "mod" {
-			let contents = std::fs::read_to_string(&path).unwrap();
+			let contents = std::fs::read_to_string(path).unwrap();
 			let mut route = String::from("");
 			let mut req_type = String::from("");
 			let mut permissions_vec: Vec<String> = vec![];
 			for line in contents.lines() {
 				if line.contains("ROUTE") && route.is_empty() {
-					let value = line.split('=').nth(1).unwrap().trim().replace("\"", "");
-					if value.ends_with(";") {
-						route = String::from(value.strip_suffix(";").unwrap());
+					let value = line.split('=').nth(1).unwrap().trim().replace('"', "");
+					if value.ends_with(';') {
+						route = String::from(value.strip_suffix(';').unwrap());
 					} else {
 						route = value;
 					}
 				}
 				if line.contains("TYPE") && req_type.is_empty() {
-					let value = line.split('=').nth(1).unwrap().trim().to_lowercase().replace("\"", "");
-					if value.ends_with(";") {
-						req_type = String::from(value.strip_suffix(";").unwrap());
+					let value = line.split('=').nth(1).unwrap().trim().to_lowercase().replace('"', "");
+					if value.ends_with(';') {
+						req_type = String::from(value.strip_suffix(';').unwrap());
 					} else {
 						req_type = value;
 					}
 				}
 				if line.contains("PERMISSIONS") && permissions_vec.is_empty() {
-					let value = line.split('=').nth(1).unwrap().trim().to_uppercase().replace("\"", "");
-					let val = if value.ends_with(";") { String::from(value.strip_suffix(";").unwrap()) } else { value };
-					permissions_vec = val.split("|").map(|p| p.trim().to_owned()).collect();
+					let value = line.split('=').nth(1).unwrap().trim().to_uppercase().replace('"', "");
+					let val = if value.ends_with(';') { String::from(value.strip_suffix(';').unwrap()) } else { value };
+					permissions_vec = val.split('|').map(|p| p.trim().to_owned()).collect();
 				}
 			}
 			if route.is_empty() {
@@ -74,8 +72,8 @@ fn main() {
 			// Extremely readable :D
 			// It takes the path, removes the file part ("./src/routes/supl/file.rs" -> "./src/routes/supl", removes the "./src" prefix -> "routes/supl", replaces all "\" with "/" (just to be sure) and replaces all "/" with "::" -> "routes::supl")
 			let modules_prefix = String::from(path.parent().unwrap().strip_prefix("./src").unwrap().to_str().unwrap())
-				.replace("\\", "/")
-				.replace("/", "::");
+				.replace('\\', "/")
+				.replace('/', "::");
 			*output += format!(
 				".route(\"{route}\", axum::routing::{req_type}({modules_prefix}::{}::callback).layer(axum::middleware::from_fn_with_state({permissions}, permissions_middleware::check_permissions)))",
 				path.file_stem().unwrap().to_str().unwrap()
@@ -87,11 +85,7 @@ fn main() {
 	read_folder(std::path::Path::new(ROUTES_FOLDER), &mut routes, &mut output, &permissions_flags);
 
 	std::fs::write(std::path::Path::new(&out_dir).join("router.rs"), format!("{{{output}}}")).unwrap();
-	std::fs::write(
-		std::path::Path::new(&out_dir).join("routes.rs"),
-		format!("const GENERATED_ROUTES: &'static str = \"{}\";", routes.join(" | ")),
-	)
-	.unwrap();
+	std::fs::write(std::path::Path::new(&out_dir).join("routes.rs"), format!("const GENERATED_ROUTES: &str = \"{}\";", routes.join(" | "))).unwrap();
 	let mut codegen_map = phf_codegen::Map::new();
 	let mut codegen_map = &mut codegen_map;
 	for (key, value) in permissions_flags.iter() {
--- api/src/routes/root.rs
+++ api/src/routes/root.rs
@@ -1,6 +1,6 @@
-pub const _ROUTE: &'static str = "/";
-pub const _PERMISSIONS: &'static str = "NONE";
-pub const _TYPE: &'static str = "GET";
+pub const _ROUTE: &str = "/";
+pub const _PERMISSIONS: &str = "NONE";
+pub const _TYPE: &str = "GET";
 
 pub async fn callback() -> &'static str {
 	"Hi"
