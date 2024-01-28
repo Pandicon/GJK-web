e9dbbcafd90377bf8137c899ce03fc403a5b86de
Pandicon <70060103+Pandicon@users.noreply.github.com>
2024-01-28 19:39:35 +0100

    [API] Apply Cargo clippy suggestions

--- api/src/auth/userdb.rs
+++ api/src/auth/userdb.rs
@@ -1,8 +1,6 @@
-
-
-const USERDB_FILE : &str = "./userdb.db";
+const USERDB_FILE: &str = "./userdb.db";
 pub struct UserDB {
-	con : sqlite::Connection
+	con: sqlite::Connection,
 }
 
 impl UserDB {
@@ -16,22 +14,22 @@ impl UserDB {
 		out
 	}
 
-	pub fn add_user(&self, mail : &str, perms : u32) -> Result<(), Box<dyn std::error::Error>> {
-		let q = format!("INSERT INTO user VALUES ('{}', {});", mail.replace("'", "''"), perms);
+	pub fn add_user(&self, mail: &str, perms: u32) -> Result<(), Box<dyn std::error::Error>> {
+		let q = format!("INSERT INTO user VALUES ('{}', {});", mail.replace('\'', "''"), perms);
 		self.con.execute(q)?;
 		Ok(())
 	}
-	pub fn remove_user(&self, mail : &str) -> Result<(), Box<dyn std::error::Error>> {
-		let q = format!("DELETE FROM user WHERE mail = '{}';", mail.replace("'", "''"));
+	pub fn remove_user(&self, mail: &str) -> Result<(), Box<dyn std::error::Error>> {
+		let q = format!("DELETE FROM user WHERE mail = '{}';", mail.replace('\'', "''"));
 		self.con.execute(q)?;
 		Ok(())
 	}
-	pub fn update_perms(&self, mail : &str, perms : u32) -> Result<(), Box<dyn std::error::Error>> {
-		let q = format!("UPDATE user SET perms = {} WHERE mail = '{}';", perms, mail.replace("'", "''"));
+	pub fn update_perms(&self, mail: &str, perms: u32) -> Result<(), Box<dyn std::error::Error>> {
+		let q = format!("UPDATE user SET perms = {} WHERE mail = '{}';", perms, mail.replace('\'', "''"));
 		self.con.execute(q)?;
 		Ok(())
 	}
-	pub fn get_perms(&self, mail : &str) -> Result<u32, Box<dyn std::error::Error>> {
+	pub fn get_perms(&self, mail: &str) -> Result<u32, Box<dyn std::error::Error>> {
 		let q = "SELECT * FROM user WHERE mail = ?;";
 		let mut s = self.con.prepare(q)?;
 		s.bind((1, mail))?;
@@ -40,7 +38,7 @@ impl UserDB {
 		}
 		Err(format!("User with mail {} doesn't exist.", mail).into())
 	}
-	pub fn get_perms_opt(&self, mail : &str) -> Result<Option<u32>, Box<dyn std::error::Error>> {
+	pub fn get_perms_opt(&self, mail: &str) -> Result<Option<u32>, Box<dyn std::error::Error>> {
 		let q = "SELECT * FROM user WHERE mail = ?;";
 		let mut s = self.con.prepare(q)?;
 		s.bind((1, mail))?;
@@ -49,7 +47,7 @@ impl UserDB {
 		}
 		Ok(None)
 	}
-	pub fn get_perms_or_add_with(&self, mail : &str, create_perms : u32) -> Result<u32, Box<dyn std::error::Error>> {
+	pub fn get_perms_or_add_with(&self, mail: &str, create_perms: u32) -> Result<u32, Box<dyn std::error::Error>> {
 		let q = "SELECT * FROM user WHERE mail = ?;";
 		let mut s = self.con.prepare(q)?;
 		s.bind((1, mail))?;
@@ -59,7 +57,7 @@ impl UserDB {
 		self.add_user(mail, create_perms)?;
 		Ok(create_perms)
 	}
-	pub fn user_exists(&self, mail : &str) -> Result<bool, Box<dyn std::error::Error>> {
+	pub fn user_exists(&self, mail: &str) -> Result<bool, Box<dyn std::error::Error>> {
 		let q = "SELECT * FROM user WHERE mail = ?;";
 		let mut s = self.con.prepare(q)?;
 		s.bind((1, mail))?;
@@ -75,8 +73,11 @@ impl UserDB {
 	}
 	pub fn print(&self) -> Result<(), Box<dyn std::error::Error>> {
 		self.con.iterate("SELECT * FROM user;", |data| {
-			tracing::info!("[{}] {}", data.iter().find(|(k, _)| k == &"perms").unwrap().1.unwrap(), 
-				data.iter().find(|(k, _)| k == &"mail").unwrap().1.unwrap());
+			tracing::info!(
+				"[{}] {}",
+				data.iter().find(|(k, _)| k == &"perms").unwrap().1.unwrap(),
+				data.iter().find(|(k, _)| k == &"mail").unwrap().1.unwrap()
+			);
 			true
 		})?;
 		Ok(())
