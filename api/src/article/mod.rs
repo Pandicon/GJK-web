19188b24df927d12eb4782607aac1e06452a6266
Pandicon <70060103+Pandicon@users.noreply.github.com>
2024-10-26 12:50:24 +0200

    Add names to users and send them with articles

--- api/src/article/mod.rs
+++ api/src/article/mod.rs
@@ -15,3 +15,29 @@ pub struct Article {
 	pub thumbnail_id: u64,
 }
 
+#[derive(Debug, Clone, serde::Serialize)]
+pub struct ArticleWithAuthorName {
+	pub id: i64,
+	pub title: String,
+	pub author_email: String,
+	pub author_name: Option<String>,
+	pub content: String,
+	pub tags: Vec<String>,
+	pub create_timestamp: u64,
+	pub thumbnail_id: u64,
+}
+
+impl ArticleWithAuthorName {
+	pub fn from_article(article: Article, author_name: Option<String>) -> Self {
+		Self {
+			id: article.id,
+			title: article.title,
+			author_email: article.author_email,
+			author_name,
+			content: article.content,
+			tags: article.tags,
+			create_timestamp: article.create_timestamp,
+			thumbnail_id: article.thumbnail_id,
+		}
+	}
+}
