be2590ad3b398358398a506349743406095f4e0b
David Frederick Batt <103059637+da-batt@users.noreply.github.com>
2024-10-30 17:56:35 +0100

    Switch to shadcn sidebar for dashboard

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -1,7 +1,18 @@
-import Sidebar from "@/components/dashboard/sidebar";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+} from "@/components/dashboard/ui/sidebar";
 import { Toaster } from "@/components/dashboard/ui/toaster";
 import { getSession, isValidSession } from "@/lib/session";
 import { redirect } from "next/navigation";
+import { NewspaperIcon, HomeIcon } from "lucide-react";
+import Link from "next/link";
 
 export default async function DashboardLayout({
   children,
@@ -14,8 +25,33 @@ export default async function DashboardLayout({
   } else {
     return (
       <>
-        <Sidebar />
-        <main className="ml-40 p-4">{children}</main>
+        <SidebarProvider>
+          <Sidebar>
+            <SidebarContent>
+              <SidebarGroup>
+                <SidebarGroupContent>
+                  <SidebarMenu>
+                    <SidebarMenuItem>
+                      <SidebarMenuButton asChild>
+                        <Link href="/dashboard/">
+                          <HomeIcon /> Domů
+                        </Link>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                    <SidebarMenuItem>
+                      <SidebarMenuButton asChild>
+                        <Link href="/dashboard/aktuality">
+                          <NewspaperIcon /> Aktuality
+                        </Link>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  </SidebarMenu>
+                </SidebarGroupContent>
+              </SidebarGroup>
+            </SidebarContent>
+          </Sidebar>
+          <main className="p-6 w-full">{children}</main>
+        </SidebarProvider>
         <Toaster />
       </>
     );
