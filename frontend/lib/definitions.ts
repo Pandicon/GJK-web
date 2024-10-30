d90814b69b9a2827de828248111afb3eb84e0597
David Frederick Batt <103059637+da-batt@users.noreply.github.com>
2024-10-30 18:11:14 +0100

    Update article type to match api

--- frontend/lib/definitions.ts
+++ frontend/lib/definitions.ts
@@ -2,7 +2,7 @@ export type Article = {
   id: number;
   create_timestamp: number;
   title: string;
-  author: string;
+  author_name: string;
   content: string;
   tags: Array<string>;
   thumbnail_id: number;
