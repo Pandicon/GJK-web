1a65db1e7633a8bf16533377e11e7b7fe70580bd
David Frederick Batt <103059637+da-batt@users.noreply.github.com>
2024-11-06 13:44:27 +0100

    Validate user permissions on new article page

--- frontend/app/dashboard/nova-aktualita/page.tsx
+++ frontend/app/dashboard/nova-aktualita/page.tsx
@@ -1,7 +1,13 @@
 import ArticleEditor from "@/components/dashboard/article-editor";
+import { UserPermission } from "@/lib/definitions";
+import { getSession } from "@/lib/session";
+import { redirect } from "next/navigation";
 
-const Page = () => {
-  return <ArticleEditor />;
+const Page = async () => {
+  const session = await getSession();
+  if (session && session.hasPermission(UserPermission.MANAGE_ARTICLES))
+    return <ArticleEditor />;
+  else redirect("/dashboard");
 };
 
 export default Page;
